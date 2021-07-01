f4c44376b72d880c16a6457241438373dfc6001d
fp024 <aopenasus@naver.com>
2021-07-01 17:31:32 +0900

    ~ p212 [06-2] 스택의 배열 기반 구현

---
+++ Chapter06/ArrayBaseStackLibrary/ArrayBaseStack.c
@@ -0,0 +1,56 @@
+﻿/**
+ * ArrayBaseStack.c
+ *
+ * 배열기반 스택 구현
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "ArrayBaseStack.h"
+
+void StackInit(Stack* pstack) {
+    pstack->topIndex = -1;     // topIndex의 -1은 빈상태를 의미한다.
+}
+
+
+int SIsEmpty(Stack* pstack) {
+    if (pstack->topIndex == -1) {
+        return TRUE;
+    }
+    else {
+        return FALSE;
+    }
+}
+
+
+void SPush(Stack* pstack, Data data) {
+    pstack->topIndex += 1;      // 데이터를 추가를 위한 인덱스 값 증가
+    pstack->stackArray[pstack->topIndex] = data;    // 데이터 저장
+}
+
+
+Data SPop(Stack* pstack) {
+    // int여서 명시적으로 배열의 데이터를 지울 필요는 없음.
+    // 동적으로 할당 받은 데이터의 경우라면 이 함수 반환 받은 사용처에서 free()하면 될 것 같다.
+
+    if (SIsEmpty(pstack)) {
+        printf("Stack Memory Error");
+        exit(-1);
+    }
+
+    int rIdx = pstack->topIndex;
+    pstack->topIndex -= 1;
+
+    return pstack->stackArray[rIdx];
+}
+
+
+
+Data SPeek(Stack* pstack) {
+    if (SIsEmpty(pstack)) {
+        printf("Stack Memory Error");
+        exit(-1);
+    }
+
+    return pstack->stackArray[pstack->topIndex];
+}
---
+++ Chapter06/ArrayBaseStackLibrary/ArrayBaseStack.h
@@ -0,0 +1,70 @@
+﻿/**
+ * ArrayBaseStack.h
+ */
+#ifndef __AB_STACK_H__
+#define __AB_STACK_H__
+
+
+#define TRUE         1
+#define FALSE        0
+#define STACK_LENGTH 100
+
+typedef int Data;
+
+typedef struct _arrayStack {
+    Data stackArray[STACK_LENGTH];
+    int topIndex;
+} ArrayStack;
+
+typedef ArrayStack Stack;
+
+/**
+ * 스택의 초기화
+ *
+ * 스택 생성 후 제일 먼저 호출되어야하는 함수
+ *
+ * @param pstack 스택 자료구조의 포인터
+ */
+void StackInit(Stack* pstack);
+
+/**
+ * 스택이 비었는지 확인
+ *
+ * @param pstack 스택 자료구조의 포인터
+ *
+ * @return 스택이 빈 경우 TRUE(1), 그렇지 않은 경우 FALSE(0)을 반환한다.
+ */
+int SIsEmpty(Stack* pstack);
+
+
+/**
+ * 스택의 push 연산
+ *
+ * @param pstack 스택 자료구조의 포인터
+ * @param data 저장할 데이터
+ */
+void SPush(Stack* pstack, Data data);
+
+/**
+ * 스택의 pop 연산
+ * top의 데이터를 꺼낸다.
+ *
+ * @param pstack 스택 자료구조의 포인터
+ *
+ * @return 스택 top의 데이터
+ */
+Data SPop(Stack* pstack);
+
+
+/**
+ * 스택의 peek 연산
+ * 스텍 top의 데이터 삭제 없이 조회한다.
+ *
+ * @param pstack 스택 자료구조의 포인터
+ *
+ * @return 스택 top의 데이터
+ */
+Data SPeek(Stack* pstack);
+
+
+#endif
