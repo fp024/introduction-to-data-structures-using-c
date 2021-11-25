3f4eb5169424fc1b54c691c1b8a073823cec63d4
fp024 <aopenasus@naver.com>
2021-11-25 19:06:21 +0900

    MinGW, Cygwin, MSVC 컴파일러 옵션 분기처리

--- cmake-projects/Chapter10/BubbleSort/BubbleSort.c
+++ cmake-projects/Chapter10/BubbleSort/BubbleSort.c
@@ -1,10 +1,12 @@
 #include "CommonUtils.h"
+#include <stdio.h>
 
 int main() {
     int numbers[] = {3, 2, 4, 1};
     int length = sizeof(numbers) / sizeof(int);
 
     // TODO
+    printf("한글\n");
 
     printArray(numbers, length);
     return 0;
